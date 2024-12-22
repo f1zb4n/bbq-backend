5758a69e595ccccd4a959cb2b9f24a23311af2a2
Marco Werner <marco.werner@dpdhl.com>
2024-12-22 17:04:01 +0100

    initial commit

---
+++ src/main/java/com/example/bbq/backend/BookToBrAInQuizBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.bbq.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookToBrAInQuizBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookToBrAInQuizBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bbq/backend/BookToBrAInQuizBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bbq.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookToBrAInQuizBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
